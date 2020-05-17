3542769a4d66cd24c224d0baa4e14ddb137f3937
Emir Sarı <bitigchi@me.com>
2020-05-17 15:11:25 +0300

    Add Turkish translation

---
+++ msgs-tr.h
@@ -0,0 +1,61 @@
+/* msgs.h - messages file
+
+   Author: Gregory Pietsch
+
+   DESCRIPTION:
+
+   This file contains #defines for all the message strings in edlin.
+   For internationalization fun, just translate the messages in this
+   file.
+
+   The strings were generated using ESR's cstrings program and moved
+   here.
+
+*/
+
+#ifndef MSGS_H
+#define MSGS_H
+
+#define YES             "Ee"
+
+#define G00001  " : "
+#define G00002  "Tamam? "
+#define G00003  "Girdi hatası"
+#define G00004  "%s: %lu satır okundu\n"
+#define G00005  "%s: %lu satır okundu\n"
+#define G00006  "%s: %lu satır yazıldı\n"
+#define G00007  "%s: %lu satır yazıldı\n"
+#define G00008  "%lu:%c%s\n"
+#define G00009  "Sürdürmek için <Enter>'a basın"
+#define G00010  "%lu: "
+#define G00011  "Bulunamadı"
+#define G00012  "%lu: %s\n"
+#define G00013  "\nedlin aşağıdaki altkomutlara iyedir:\n"
+#define G00014  "#                 tek bir satır düzenle [#],[#],#m        taşı"
+#define G00015  "a                 iliştir               [#][,#]p          sayfa"
+#define G00016  "[#],[#],#,[#]c    kopyala               q                 çık"
+#define G00017  "[#][,#]d          sil                   [#][,#][?]r$,$    değiştir"
+#define G00018  "e<>               son (yaz ve çık)      [#][,#][?]s$      ara"
+#define G00019  "[#]i              ekle                  [#]t<>            aktar"
+#define G00020  "[#][,#]l          listele               [#]w<>            yaz\n"
+#define G00021  "$ bir dizi, <> bir dosya adı iken,"
+#define G00022  "# bir sayı (.=geçerli satır, $=son satır,"
+#define G00023  "veya her bir sayı + veya - başka bir sayı).\n"
+#define G00024  ", telif hakkı (c) 2003 Gregory Pietsch"
+#define G00025  "Bu program herhangi bir GARANTİ İLE GELMEZ."
+#define G00026  "Bu program özgür yazılım olup GNU Genel Kamu Lisansının"
+#define G00027  "(GNU General Public License) 2. sürümü veya daha yeni "
+#define G00028  "bir sürümü altında özgürce dağıtılabilir."
+#define G00029  "İstediğiniz sürümü seçebilirsiniz.\n"
+#define G00030  "Yetersiz bellek"
+#define G00031  "Dizi uzunluk hatası"
+#define G00032  "Dizi konum hatası"
+#define G00033  "Geçersiz girdi, yardım için ? yapın."
+#define G00034  "Dosya adı yok"
+#define G00035  "Arabellek çok büyük"
+#define G00036  "Geçersiz arabellek konumu"
+#define G00037  "HATA: %s\n"
+
+#endif
+
+/* END OF FILE */
